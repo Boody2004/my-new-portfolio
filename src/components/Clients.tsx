a29840aedbfcddab5148a0f717308847d48a908f
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2024-06-29 17:59:56 +0300

    add Approach

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -7,14 +7,14 @@ const Clients = () => {
       <h1 className="heading">
         Kind words from <span className="text-purple"> satisfied clients</span>
       </h1>
-      <div className="flex flex-col items-center max-lg:mt-10">
+      <div className="flex flex-col items-center lg:mt-10">
         <InfiniteMovingCards
           items={testimonials}
           direction="right"
           speed="slow"
         />
 
-        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
+        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 lg:mt-10">
           {companies.map(({ id, img, name, nameImg }) => (
             <div className="flex md:max-w-60 max-w-32 gap-2">
               <img src={img} alt={name} className="md:w-10 w-5" />

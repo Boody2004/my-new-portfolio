fc9e64bf0c9e402c5510c828323a3e407766cf77
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2024-06-30 18:05:40 +0300

    edit com and add new page (showcase)

---
+++ app/showcase/page.tsx
@@ -0,0 +1,12 @@
+import RecentProjects from "@/components/RecentProjects";
+import React from "react";
+
+const showcase = () => {
+  return (
+    <div>
+      <RecentProjects />
+    </div>
+  );
+};
+
+export default showcase;
